028b0cb6b0dde8c6d3bf0d9ab09acf88ea9f0475
kimjungjin <jungks9351@gmail.com>
2022-08-10 17:07:07 +0900

    chore : react-router-dom 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import GlobalStyles from './styles/GlobalStyles';
 
-import SearchPage from './pages/SearchPage';
 import MoviePage from './pages/MoviePage';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <>
       <GlobalStyles />
-      <Routes>
-        <Route path='/search' element={<SearchPage />} />
-        <Route path='/' element={<MoviePage />} />
-      </Routes>
-    </BrowserRouter>
+      <MoviePage />
+    </>
   );
 };
 
